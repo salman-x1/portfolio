98f22a45b21640bee1e563cc831c1cddc7963434
salman-x1 <salmannazir2001@gmail.com>
2024-07-31 04:24:26 +0500

    Portfolio

--- src/main.js
+++ src/main.js
@@ -14,5 +14,7 @@ import "../styles/utils.css";
 import './utils/mobile-nav.js'
 import mobileNav from "./utils/mobile-nav.js";
 import darkMode from "./utils/dark-mode.js";
+import lazyLoading from "./utils/lazy-loading.js";
 mobileNav();
 darkMode(); 
+lazyLoading();
